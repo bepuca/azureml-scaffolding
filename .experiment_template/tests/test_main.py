606705854081d412025b547b36c964721349fd8b
bepuca <bernatpuig@gmail.com>
2025-04-02 14:20:04 +0000

    First try of experiments

--- .experiment_template/tests/test_main.py
+++
@@ -1,9 +0,0 @@
-from main import get_the_ultimate_answer
-
-
-def test_get_the_ultimate_answer():
-    expected_answer = 42
-
-    answer = get_the_ultimate_answer()
-
-    assert answer == expected_answer
