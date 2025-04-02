0b52cbd3414f855eed1195fa12dceefedbe54f11
bepuca <bernatpuig@gmail.com>
2025-04-02 11:34:35 +0000

    Create pipeline template artifacts

--- .pipeline-template/reader-step/__main__.py
+++ .pipeline-template/reader-step/__main__.py
@@ -1,21 +1,22 @@
 from pathlib import Path
 
 
-def fun(data_path: Path):
-    file = data_path / "hello.txt"
+def read(data_path: Path):
+    """Read "file.txt" in `data_path` and print its contents."""
+    file = data_path / "file.txt"
     print(file.read_text())
 
 
 def main():
     import argparse
+    import os
     import sys
-    from pathlib import Path
 
     assume_debug = len(sys.argv) <= 1
     if assume_debug:
         print("WARNING: Using debug args because no args were passed")
         args_dict = {
-            "data_path": Path("/workspaces/azure-mlops-scaffolding/data/outputs"),
+            "data_path": Path(os.environ["REPO_ROOT"]) / "data/outputs",
         }
     else:
         parser = argparse.ArgumentParser(description="Driver script for Example Package")
@@ -24,7 +25,7 @@ def main():
         args = parser.parse_args()
         args_dict = vars(args)
 
-    fun(**args_dict)
+    read(**args_dict)
 
 
 if __name__ == "__main__":
--- .pipeline-template/writer-step/__main__.py
+++ .pipeline-template/writer-step/__main__.py
@@ -1,31 +1,34 @@
 from pathlib import Path
 
 
-def fun(data_path: Path):
+def write(content: str, data_path: Path):
+    """Write `content` to "file.txt" in `data_path`"""
     data_path.mkdir(parents=True, exist_ok=True)
-    file = data_path / "hello.txt"
-    file.write_text("Houston we do not have a problem")
+    file = data_path / "file.txt"
+    file.write_text(content)
 
 
 def main():
     import argparse
+    import os
     import sys
-    from pathlib import Path
 
     assume_debug = len(sys.argv) <= 1
     if assume_debug:
         print("WARNING: Using debug args because no args were passed")
         args_dict = {
-            "data_path": Path("/workspaces/azure-mlops-scaffolding/data/outputs"),
+            "content": "Houston we do not have a problem!",
+            "data_path": Path(os.environ["REPO_ROOT"]) / "data/outputs",
         }
     else:
         parser = argparse.ArgumentParser(description="Driver script for Example Package")
+        parser.add_argument("--content", type=str, help="Content to write", required=True)
         parser.add_argument("--data_path", type=Path, help="Path to data", required=True)
 
         args = parser.parse_args()
         args_dict = vars(args)
 
-    fun(**args_dict)
+    write(**args_dict)
 
 
 if __name__ == "__main__":
