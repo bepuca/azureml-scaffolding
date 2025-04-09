541d9197e6cb69b1d59be1bedfbee09652876e1c
bepuca <bernatpuig@gmail.com>
2025-04-09 14:50:08 +0000

    Add invalid usage util

---
+++ bin/utils/utils.sh
@@ -0,0 +1,11 @@
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+    echo >&2 "This script must be sourced, not executed."
+    exit 1
+fi
+
+
+utils::invalid_usage() {
+    echo >&2 "$1"
+    usage >&2   # Must be defined in script that sources this one
+    exit 1
+}
