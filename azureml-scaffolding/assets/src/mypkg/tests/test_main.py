cdaa1937ad46cf8bfee7c7913ac4642eab7b7d22
bepuca <44361528+bepuca@users.noreply.github.com>
2026-02-17 15:47:14 +0100

    test experiment commit with checkout and push

---
+++ azureml-scaffolding/assets/src/mypkg/tests/test_main.py
@@ -0,0 +1,6 @@
+from mypkg.__main__ import get_ultimate_answer
+
+
+def test_get_ultimate_answer():
+    """Illustrative purposes"""
+    assert 42 == get_ultimate_answer()
--- packages/example-reader-step/README.md
+++
@@ -1,1 +0,0 @@
-# example_reader_step
